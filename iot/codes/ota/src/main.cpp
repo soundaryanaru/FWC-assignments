48501db0e50fdb564383d16d905f054f73bad12a
soundaryanaru <97998905+soundaryanaru@users.noreply.github.com>
2022-11-03 20:10:05 +0530

    Add files via upload

---
+++ iot/codes/ota/src/main.cpp
@@ -0,0 +1,55 @@
+//----------------------Skeleton Code--------------------//
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+//    Can be client or even host   //
+#ifndef STASSID
+#define STASSID "Soundarya" // Add your network credentials
+#define STAPSK  "nithya@123"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+int U,V,W,A;
+void OTAsetup() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    delay(5000);
+    ESP.restart();
+  }
+  ArduinoOTA.begin();
+}
+
+void OTAloop() {
+  ArduinoOTA.handle();
+}
+
+//-------------------------------------------------------//
+
+void setup(){
+  OTAsetup();
+  pinMode(2, OUTPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+
+  //-------------------//
+  // Custom setup code //
+  //-------------------//
+}
+
+void loop() {
+  OTAloop();
+  U = digitalRead(6);
+  V = digitalRead(7);
+  W= digitalRead(8);
+  A = (U&&V) || (U&&!W);
+  digitalWrite(2,A)
+  delay(10);  // If no custom loop code ensure to have a delay in loop
+  //-------------------//
+  // Custom loop code  //
+  //-------------------//
+}
